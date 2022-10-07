1ceed9ac8ede0f79425bfbc2ec3afd702eacbdce
zulfffia <zulfiavaninova@gmail.com>
2022-10-07 22:33:34 +0600

    xgge

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
-// цикл for
-for (let i = 0; i < 5; i++){
+// // цикл for
+// for (let i = 0; i < 5; i++){
+//     console.log(i)
+// }
+// //объекты
+// const users = {
+//     name:  {
+//         zulfia: {
+//             age: 15,
+//             sex: 'girl'
+//         },
+//         kamiliya: {
+//         age: 18,
+//         sex: 'girl'
+//         },
+//         nooruz: {
+//             age: 18,
+//             sex: 'boy'
+//         }
+//     }
+// }
+// console.log(users)
+// //массивы
+// const products = ['milk', 'bread','tea']
+// console.log(products
+
+//№1
+//введите число от 1 до 50 в столбец
+for (let i = 0; i < 51; i++){
     console.log(i)
 }
-//объекты
-const users = {
-    name:  {
-        zulfia: {
-            age: 15,
-            sex: 'girl'
-        },
-        kamiliya: {
-        age: 18,
-        sex: 'girl'
-        },
-        nooruz: {
-            age: 18,
-            sex: 'boy'
-        }
-    }
-}
-console.log(users)
-//массивы
-const products = ['milk', 'bread','tea']
-console.log(products)
+
+//№2
+//Необходимо вывести на экран фразу 'ПРИВЕТ, МИР!'.
+let a = ['Привет,', 'мир', '!'];
+console.log(a [0] + a [1] + a [2])
+
 
