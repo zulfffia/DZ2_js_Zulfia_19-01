9c9d2754b6e2073ac3ec4ae377b36f5585780301
zulfffia <zulfiavaninova@gmail.com>
2022-10-07 19:25:50 +0600

    'dz'

---
+++ app.js
@@ -0,0 +1,24 @@
+// цикл for
+for (let i = 0; i < 5; i++){
+    console.log(i)
+}
+//объекты
+const users = {
+    zulfia: {
+        age: 15,
+        sex: 'girl'
+    },
+    kamiliya: {
+        age: 18,
+        sex: 'girl'
+    },
+    nooruz: {
+        age: 18,
+        sex: 'boy'
+    }
+}
+console.log(users)
+//массивы
+const products = ['milk', 'bread','tea']
+console.log(products)
+
